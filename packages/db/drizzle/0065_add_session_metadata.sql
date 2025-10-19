8554a662fc6f4ae7af7fcc6003f89fed67836af5
Mohamed Bassem <me@mbassem.com>
2025-10-19 19:35:10 +0000

    drop schema changes

--- packages/db/drizzle/0065_add_session_metadata.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `session` ADD `createdAt` integer NOT NULL;--> statement-breakpoint
-ALTER TABLE `session` ADD `modifiedAt` integer;--> statement-breakpoint
-ALTER TABLE `session` ADD `ipAddress` text;--> statement-breakpoint
-ALTER TABLE `session` ADD `userAgent` text;
