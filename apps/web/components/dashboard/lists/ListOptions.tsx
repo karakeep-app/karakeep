a58a95f1397ac8fa2719124988e067470c897832
Mohamed Bassem <me@mbassem.com>
2025-11-16 19:49:02 +0000

    fix alignment of leave list

--- apps/web/components/dashboard/lists/ListOptions.tsx
+++ apps/web/components/dashboard/lists/ListOptions.tsx
@@ -116,7 +116,7 @@ export function ListOptions({
       icon: <DoorOpen className="size-4" />,
       visible: isCollaborator,
       disabled: false,
-      className: "text-destructive",
+      className: "flex gap-2 text-destructive",
       onClick: () => setLeaveListDialogOpen(true),
     },
     {
@@ -125,7 +125,7 @@ export function ListOptions({
       icon: <Trash2 className="size-4" />,
       visible: isOwner,
       disabled: false,
-      className: "text-destructive",
+      className: "flex gap-2 text-destructive",
       onClick: () => setDeleteListDialogOpen(true),
     },
   ];
