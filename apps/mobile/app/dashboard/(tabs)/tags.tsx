32d46b0a2e60aff08d0d6ea87e59f892898a6d94
MohamedBassem <me@mbassem.com>
2025-11-23 11:03:13 +0000

    format

--- apps/mobile/app/dashboard/(tabs)/tags.tsx
+++ apps/mobile/app/dashboard/(tabs)/tags.tsx
@@ -92,7 +92,12 @@ export default function Tags() {
         }}
         renderItem={(item) => (
           <View className="mx-2 flex flex-row items-center rounded-xl border border-input bg-card px-4 py-2">
-            <Link asChild key={item.item.id} href={item.item.href} className="flex-1">
+            <Link
+              asChild
+              key={item.item.id}
+              href={item.item.href}
+              className="flex-1"
+            >
               <Pressable className="flex flex-row justify-between">
                 <View className="flex-1">
                   <Text className="font-medium">{item.item.name}</Text>
